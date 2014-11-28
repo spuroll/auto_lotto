10bd601738f744621bfe88ae996d5bc3717bf598
Steven <stevenpuroll@gmail.com>
2014-11-28 06:33:40 +0000

    Define actions in NumberSet controller

--- app/controllers/number_set_controller.rb
+++ app/controllers/number_set_controller.rb
@@ -1,16 +1,21 @@
 class NumberSetController < ApplicationController
   def index
+    @sets = NumberSet.all
   end
 
   def show
+    @set = NumberSet.find(params[:id])
   end
 
   def new
+    @set = NumberSet.new
   end
 
   def edit
+    @set = NumberSet.find(params[:id])
   end
 
   def delete
+    @set = NumberSet.find(params[:id])
   end
 end
