26d4c2931be366e5fa086d36631c6033c57a4cf2
Steven <stevenpuroll@gmail.com>
2014-11-29 04:31:26 +0000

    Add create action to email controller

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,12 +1,21 @@
 class EmailController < ApplicationController
 
   def create
-  
+    @email = Email.new(email_params)
+    @email.save
+    redirect_to(:controller => 'number_set', :action => 'index')  
   end
   
   def destroy
     Email.find(params[:id]).destroy
     redirect_to(:controller => 'number_set', :action => 'index')
   end
+
+  private
+  
+  def email_params
+     params.require(:email).permit(:email, :number_set_id) 
+  end
+
 
 end
