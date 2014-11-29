f27aa4fa7466a83a116c595e75d542a2b8e22c5c
Steven <stevenpuroll@gmail.com>
2014-11-29 02:20:51 +0000

    Troubleshoot heroku issues

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'number_set/delete'
 
+  get    'index' => 'number_set#index'
   get    'new'   => 'number_set#new'
   post   'new'   => 'number_set#create'
 
