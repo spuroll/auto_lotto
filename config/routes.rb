cd9e46d9c8fcba8ad9a96f318777100dc302bc92
Steven <stevenpuroll@gmail.com>
2014-11-29 02:53:17 +0000

    Troubleshoot heroku

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,11 @@ Rails.application.routes.draw do
 
   get 'number_set/show'
 
+  get 'number_set/new'
+
   get 'number_set/edit'
 
   get 'number_set/delete'
-
-  get    'index' => 'number_set#index'
-  get    'new'   => 'number_set#new'
-  post   'new'   => 'number_set#create'
 
   match ':controller(/:action(/:id))', :via => [:get, :post]
 
