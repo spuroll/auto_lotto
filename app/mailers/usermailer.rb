ded8cd05dcd3cf65935df9cd40d904dda181dbac
Steven <stevenpuroll@gmail.com>
2014-11-29 21:49:39 +0000

    Add 'from' value to troubleshoot Postmark

--- app/mailers/usermailer.rb
+++ app/mailers/usermailer.rb
@@ -1,5 +1,5 @@
 class Usermailer < ActionMailer::Base
   def task_email
-    mail(to: "stevenpuroll@gmail.com", subject: "Task Working")   
+    mail(to: "stevenpuroll@gmail.com", from: "steven@stevenpuroll.com", subject: "Task Working")   
   end
 end
