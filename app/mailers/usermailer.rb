4e6f5d4249bc8f838471e60372f8bcb70ef162da
Steven <stevenpuroll@gmail.com>
2014-12-02 05:02:39 +0000

    Edit Usermailer to include compare_email

--- app/mailers/usermailer.rb
+++ app/mailers/usermailer.rb
@@ -1,5 +1,9 @@
 class Usermailer < ActionMailer::Base
-  def task_email
-    mail(to: "stevenpuroll@gmail.com", from: "steven@stevenpuroll.com", subject: "Did you win?")   
+  def compare_email(email, check, last_draw, checkstring)
+    @email = email
+    @check = check
+    @last_draw = last_draw
+    @checkstring = checkstring
+    mail(to: email, from: "steven@stevenpuroll.com", subject: "Did you win?")   
   end
 end
