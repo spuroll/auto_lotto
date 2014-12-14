0c003e20dad7f0ea0683a83dd45f05a537450d12
Steven <stevenpuroll@gmail.com>
2014-12-14 01:04:07 +0000

    Refactor: move last_draw display logic from controller to helper

--- app/controllers/number_set_controller.rb
+++ app/controllers/number_set_controller.rb
@@ -12,55 +12,10 @@ class NumberSetController < ApplicationController
         @active_sets << set
       end 
     end
-
-  require 'open-uri'
-  file = open('http://www.megamillions.com/winning-numbers')
-  contents = file.read
-  contents.to_s
-  startparse = contents.index('<div class="ball white">') + 22
-  endparse = contents.index('<div class="ball gold">') + 30
-  substring = contents[startparse..endparse]
-  
-  start_array = Array.new
-  
-  substring.to_enum(:scan,/">/).map do |m,|
-      start_array << [$`.size,m]
-  end
-  
-  start_one = start_array[0][0] + 2
-  start_two = start_array[1][0] + 2
-  start_three = start_array[2][0] + 2
-  start_four = start_array[3][0] + 2
-  start_five = start_array[4][0] + 2
-  start_six = start_array[5][0] + 2
-  
-  end_array = Array.new
-  substring.to_enum(:scan,/<\/div>/).map do |m,|
-      end_array << [$`.size,m]
-  end
-  
-  end_one = end_array[0][0] -1
-  end_two = end_array[1][0] -1
-  end_three = end_array[2][0] -1
-  end_four = end_array[3][0] -1
-  end_five = end_array[4][0] -1
-  end_six = end_array[5][0] -1
-  
-  num_one = substring[start_one..end_one]
-  num_two = substring[start_two..end_two]
-  num_three = substring[start_three..end_three]
-  num_four = substring[start_four..end_four]
-  num_five = substring[start_five..end_five]
-  num_six = substring[start_six..end_six]
-  
-  @last_draw = "#{num_one} #{num_two} #{num_three} #{num_four} #{num_five} #{num_six}"
-      
   end
 
   def show
-    
     @set = NumberSet.find(params[:id])
-    
   end
   
   def expired
@@ -91,7 +46,6 @@ class NumberSetController < ApplicationController
         flash[:notice] = "Please ensure you've entered values in the correct formats."
         render('new')
       end
-
   end
 
   def edit
--- app/helpers/number_set_helper.rb
+++ app/helpers/number_set_helper.rb
@@ -1,3 +1,49 @@
 module NumberSetHelper
+  
+    def last_draw()
+      require 'open-uri'
+      file = open('http://www.megamillions.com/winning-numbers')
+      contents = file.read
+      contents.to_s
+      startparse = contents.index('<div class="ball white">') + 22
+      endparse = contents.index('<div class="ball gold">') + 30
+      substring = contents[startparse..endparse]
+      
+      start_array = Array.new
+      
+      substring.to_enum(:scan,/">/).map do |m,|
+          start_array << [$`.size,m]
+      end
+      
+      start_one = start_array[0][0] + 2
+      start_two = start_array[1][0] + 2
+      start_three = start_array[2][0] + 2
+      start_four = start_array[3][0] + 2
+      start_five = start_array[4][0] + 2
+      start_six = start_array[5][0] + 2
+      
+      end_array = Array.new
+      
+      substring.to_enum(:scan,/<\/div>/).map do |m,|
+          end_array << [$`.size,m]
+      end
+      
+      end_one = end_array[0][0] -1
+      end_two = end_array[1][0] -1
+      end_three = end_array[2][0] -1
+      end_four = end_array[3][0] -1
+      end_five = end_array[4][0] -1
+      end_six = end_array[5][0] -1
+      
+      num_one = substring[start_one..end_one]
+      num_two = substring[start_two..end_two]
+      num_three = substring[start_three..end_three]
+      num_four = substring[start_four..end_four]
+      num_five = substring[start_five..end_five]
+      num_six = substring[start_six..end_six]
+      
+      return "#{num_one} #{num_two} #{num_three} #{num_four} #{num_five} #{num_six}"
+       
+    end
     
 end
