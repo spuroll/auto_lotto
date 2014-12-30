0d0b7631632a6e418f89c4c2a6097806c487cd44
Steven <stevenpuroll@gmail.com>
2014-12-30 04:20:22 +0000

    Cleared auto-generated tests. Added first custom test to suite

--- test/models/number_set_test.rb
+++ test/models/number_set_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class NumberSetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @number_set = NumberSet.new(set: "45 87 65 48 45 98", check_date: "2015-01-12")
+  end
+  
+  test "should be valid" do
+    assert @number_set.valid?
+  end
 end
